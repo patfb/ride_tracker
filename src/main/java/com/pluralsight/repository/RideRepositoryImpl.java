77f98597985759ce58c2ab6e48e9128a13d9d75b
Patrick <patrick.f.boyne@gmail.com>
2018-05-14 00:33:01 -0400

    creating records in the database complete

--- src/main/java/com/pluralsight/repository/RideRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/RideRepositoryImpl.java
@@ -1,17 +1,20 @@
 package com.pluralsight.repository;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import com.pluralsight.model.Ride;
 
 @Repository("rideRepository")
 public class RideRepositoryImpl implements RideRepository {
-	
+
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 
@@ -20,9 +23,33 @@ public class RideRepositoryImpl implements RideRepository {
 		Ride ride = new Ride();
 		ride.setName("Corner Canyon");
 		ride.setDuration(120);
-		List <Ride> rides = new ArrayList<>();
+		List<Ride> rides = new ArrayList<>();
 		rides.add(ride);
 		return rides;
 	}
-	
+
+	@Override
+	public Ride createRide(Ride ride) {
+		jdbcTemplate.update("insert into ride (name, duration) values (?,?)", ride.getName(), ride.getDuration());
+		// SimpleJdbcInsert insert = new SimpleJdbcInsert(jdbcTemplate);
+		// List<String> columns = new ArrayList<>();
+		// columns.add("name");
+		// columns.add("duration");
+		//
+		// insert.setTableName("ride");
+		// insert.setColumnNames(columns);
+		//
+		// Map<String, Object> data = new HashMap<>();
+		// data.put("name", ride.getName());
+		// data.put("duration", ride.getDuration());
+		//
+		// insert.setGeneratedKeyName("id");
+		//
+		// Number key = insert.executeAndReturnKey(data);
+		//
+		// System.out.println(key);
+
+		return null;
+	}
+
 }
--- src/test/java/com/pluralsight/controller/RestControllerTest.java
+++ src/test/java/com/pluralsight/controller/RestControllerTest.java
@@ -13,13 +13,24 @@ import org.junit.Test;
 
 public class RestControllerTest {
 
-	@Test(timeout=3000)
+	@Test(timeout = 3000)
+	public void testCreateRide() {
+		RestTemplate restTemplate = new RestTemplate();
+
+		Ride ride = new Ride();
+		ride.setName("Round Valley Ride");
+		ride.setDuration(38);
+
+		restTemplate.put("http://localhost:8080/ride_tracker/ride", ride);
+
+	}
+
+	@Test(timeout = 3000)
 	public void testGetRides() {
 		RestTemplate restTemplate = new RestTemplate();
 
-		ResponseEntity<List<Ride>> ridesResponse = restTemplate.exchange(
-				"http://localhost:8080/ride_tracker/rides", HttpMethod.GET,
-				null, new ParameterizedTypeReference<List<Ride>>() {
+		ResponseEntity<List<Ride>> ridesResponse = restTemplate.exchange("http://localhost:8080/ride_tracker/rides",
+				HttpMethod.GET, null, new ParameterizedTypeReference<List<Ride>>() {
 				});
 		List<Ride> rides = ridesResponse.getBody();
 
